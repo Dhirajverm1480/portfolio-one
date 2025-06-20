071b7359d8d593d97833ad48c178484172219e25
Dhiraj Verma <144448841+Dhirajverm1480@users.noreply.github.com>
2025-06-20 13:23:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 let menuImg = document.getElementById('menu-img')
 let menuBar = document.querySelector('.menu-bar');
+let menuBox = document.querySelector('.menu-box')
+let Close = document.getElementById("close")
+
+menuBar.addEventListener('click', () => {
+    menuBox.style.display = 'block'
+})
+
+Close.addEventListener('click', () => {
+    menuBox.style.display = 'none'
+})
